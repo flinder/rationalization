803054cbcb230beedec22db6c87e7415dfc2bab2
flinder <fridolin.linder@gmail.com>
2014-09-20 10:36:42 -0400

    Simulate confidence intervals for ratio

--- power_analysis.R
+++ power_analysis.R
@@ -4,6 +4,18 @@
 
 rm(list=ls())
 library(ggplot2)
+library(truncnorm)
+
+# Getting realisitc estimates for pars of distribution of self position
+setwd("C:/Users/samsung/Dropbox/rationalization/rationalization")
+anes = read.table('data/anes.csv')
+
+as = anes$libcpre_self
+as_norm = (as - min(as))/(max(as)-min(as))
+plot(density(as_norm,bw=0.12,from=0,to=1),lwd=2)
+x = seq(0,1,by=0.01)
+points(x,dtruncnorm(x,0,1,0.5,0.3),type='l',lty=2,lwd=2,col='red')
+
 
 ### Function simulating the data generating process for simple case:
 ### Only self position is biased and party position is assumed to be 
@@ -29,7 +41,7 @@ library(ggplot2)
 
 genData = function(N,md=0.5,p1=0.2,p2=0.8,v=0.1){
   require(truncnorm)
-  ts = rtruncnorm(N,0,1,0.5,0.3) # true self position
+  ts = rtruncnorm(N,0,1,0.5,0.2) # true self position
   pp = cbind(rtruncnorm(N,0,1,p1,v),rtruncnorm(N,0,1,p2,v)) # perc part pos
   dtf = apply(pp-ts,1,function(x) x[which.min(abs(x))]) # distance to favorite
   fp = apply(abs(pp-ts),1,which.min) # favorite party
@@ -48,14 +60,6 @@ hist(dist[X$group==0],breaks=30,col='grey')
 hist(dist[X$group==1],breaks=30,col='grey')
 mean(dist[X$group==1])/mean(dist[X$group==0])
 
-
-### Power analysis by simulation
-
-# Assumed effect size (in terms of md)
-md = as.list(seq(0.05,1,by=0.05))
-N = 100
-B = 1000
-
 # Function to generate data and calcualte difference in mean abs distance
 diff = function(md,N,ratio=T){
   X = genData(N,md=md)
@@ -64,8 +68,22 @@ diff = function(md,N,ratio=T){
     else mean(dist[X$group==1])/mean(dist[X$group==0])
 }
 
-# Sample difference
+# Function to Sample difference/ratio of means
 sampDiff = function(B,N,md) sapply(rep(md,B),diff,N=N)
 
-# Get dist'ns for different md vals
-distns = lapply(md,sampDiff1,B=B,N=N)
+
+### Power analysis by simulation
+cis = list()
+d = c(0.05,0.1,0.2,0.5)
+N = list(10,100,200,500,1000)
+B = 1000
+
+for(i in 1:length(d)){
+  print(d[i])
+  md = d[i] # Reduction of distance by 100*d[i]%
+  distns = lapply(N,sampDiff,B=B,md=md)
+  ci = do.call(rbind,lapply(distns,quantile,c(0.025,0.975)))
+  cis[[i]] = ci
+}
+
+save(file='data/ci.RData','cis')
